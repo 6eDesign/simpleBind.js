56a50c7c1f7445b34f94ea0ade9cc4b3824aeeef
Jonathan Greenemeier <jgreenemeier@homeadvisor.com>
2017-09-05 15:55:28 -0600

    Support tel phone type

--- src/bindTypes/simplebindvalue.js
+++ src/bindTypes/simplebindvalue.js
@@ -80,6 +80,7 @@ simpleBind = (function(w,d,util,pub){
   var attachAppropriateEventHandlers = function(elem,inputType) {
     switch(inputType) {
       case 'text':
+      case 'tel':
       case 'password':
       case 'textarea':
       case 'zip':
