30e93f5f5f45d6b595de6899a0b4f97b101b348f
Jonathan Greenemeier <jgreenemeier@homeadvisor.com>
2018-04-04 12:31:47 -0600

    bindhandler update

--- src/bindTypes/simplebindhandler.js
+++ src/bindTypes/simplebindhandler.js
@@ -20,10 +20,11 @@ simpleBind = (function(w,d,util,pub){
 
   var bindingRoutine = function(config,obj,flush){
     // binding routine, the function that determines how binding is done for this bind type
+    if(isTarget) console.log(config);
     if(typeof state.bindHandlers[config.handler] != 'undefined') {
       var val = util.get(obj,config.objKey)
         , oldVal = util.get(state.boundObjectsLast[config.objName],config.objKey);
-      var change = typeof val == 'object' ? JSON.stringify(val) != JSON.stringify(oldVal) : val != oldVal;
+      var change = typeof val == 'object' ? JSON.stringify(val) != JSON.stringify(oldVal) : val !== oldVal;
       if(change || flush) {
         state.bindHandlers[config.handler](config.elem,util.get(obj,config.objKey),config.objName);
       }
