956c2e64bf1d5413681fa20fe0b60aa064e1d22c
Jonathan Greenemeier <jgreenemeier@homeadvisor.com>
2018-04-04 12:41:34 -0600

    Oops

--- src/bindTypes/simplebindhandler.js
+++ src/bindTypes/simplebindhandler.js
@@ -20,7 +20,6 @@ simpleBind = (function(w,d,util,pub){
 
   var bindingRoutine = function(config,obj,flush){
     // binding routine, the function that determines how binding is done for this bind type
-    if(isTarget) console.log(config);
     if(typeof state.bindHandlers[config.handler] != 'undefined') {
       var val = util.get(obj,config.objKey)
         , oldVal = util.get(state.boundObjectsLast[config.objName],config.objKey);
